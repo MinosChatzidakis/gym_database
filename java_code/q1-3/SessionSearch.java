0200ea7b14c56daa9eb82e56c5e6e5f7f531cf9d
Aggelos06 <aggeloskarahalis@gmail.com>
2026-06-04 18:06:36 +0300

    Add files via upload

---
+++ java_code/q1-3/SessionSearch.java
@@ -0,0 +1,37 @@
+package Test;
+
+public class SessionSearch {
+	private String city;
+    private int preferredGymCode;
+    private String trainingType;
+   
+    public SessionSearch(int preferredGymCode, String city, String trainingType) {
+        this.preferredGymCode = preferredGymCode;
+        this.city = city;
+        this.trainingType = trainingType;
+        
+    }
+
+    public int getPreferredGymCode() {
+        return preferredGymCode;
+    }
+
+    public void setPreferredGymCode(int preferredGymCode) {
+        this.preferredGymCode = preferredGymCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getTrainingType() { 
+        return trainingType; 
+    }
+    public void setTrainingType(String trainingType) {
+        this.trainingType = trainingType;
+    }
+
+}
