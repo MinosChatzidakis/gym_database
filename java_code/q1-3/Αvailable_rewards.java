c4f0fb06fbac1f0769573abd1422c3d3d4ac1387
Aggelos Karahalis <aggeloskarahalis@Aggeloss-MacBook-Air.local>
2026-06-04 17:15:11 +0300

    uploading classes

---
+++ java_code/q1-3/Αvailable_rewards.java
@@ -0,0 +1,48 @@
+package Test;
+
+public class Αvailable_rewards {
+	private int reward_id;
+    private String description;
+    private int valid_for;
+    private int pts_required;
+
+
+    public Αvailable_rewards(int reward_id, String description, int valid_for, String phone, int pts_required) {
+        this.reward_id = reward_id;
+        this.description = description;
+        this.valid_for = valid_for;
+        this.pts_required = pts_required;
+    }
+
+    public int getReward_id() {
+        return reward_id;
+    }
+
+    public void setReward_id(int reward_id) {
+        this.reward_id = reward_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPts_required() {
+        return pts_required;
+    }
+
+    public void setPts_required(int pts_required) {
+        this.pts_required = pts_required;
+    }
+
+    public int getValid_for() {
+        return valid_for; 
+    }
+
+    public void setValid_for(int valid_for) {
+        this.valid_for = valid_for; 
+    }
+}
