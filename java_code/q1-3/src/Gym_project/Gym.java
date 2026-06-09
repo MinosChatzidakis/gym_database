a27cae3181be62e6907404ffb215cba179f135bb
Aggelos06 <aggeloskarahalis@gmail.com>
2026-06-09 16:13:13 +0300

    Update Gym.java

--- java_code/q1-3/src/Gym_project/Gym.java
+++ java_code/q1-3/src/Gym_project/Gym.java
@@ -2,17 +2,56 @@ package Gym_project;
 
 public class Gym {
 	
+	public int gymCode;
     private String name;
+	private String address;
+	private String city;
+	private String phone;
     private String email;
-    private String phone;
-    public int gymCode;
+	private String services;
 
-
-    public Gym(String name, String email, String phone, int gymCode) {
+    public Gym(String city , String services , String address , String name, String email, String phone, int gymCode) {
+		this.city = city;
+		this.services = services;
+		this.address = address;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.gymCode = gymCode;
+    }
+
+	public String getCity() {
+        return city;
+    }
+
+	public void setCity(String city) {
+		if(city==null || city.lenght() == 0){
+			throw new IllegalArgumentException("The city field cant be empty.");
+		}	
+        this.city = city;
+    }
+
+	
+	public String getServices() {
+        return services;
+    }
+
+	public void setServices(String services) {
+		if(services==null || services.lenght() == 0){
+			throw new IllegalArgumentException("The services field cant be empty.");
+		}	
+        this.services = services;
+    }
+	
+	public String getAddress() {
+        return address;
+    }
+
+	public void setAddress(String address) {
+		if(address==null || address.lenght() == 0){
+			throw new IllegalArgumentException("The address field cant be empty.");
+		}	
+        this.address = address;
     }
 
     public String getName() {
