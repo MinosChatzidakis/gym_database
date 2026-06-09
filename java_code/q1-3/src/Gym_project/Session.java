1809c73588d14dd73c3b719d264f04c8e411ea2c
Aggelos06 <aggeloskarahalis@gmail.com>
2026-06-09 16:36:39 +0300

    Update Session.java

--- java_code/q1-3/src/Gym_project/Session.java
+++ java_code/q1-3/src/Gym_project/Session.java
@@ -10,8 +10,10 @@
 	    private boolean Availability;
 	    private int Trainer_Trainer_id;
 	    private int GYM_Gym_code;
+		private String date_And_Time;
+		private int amount_Of_Participants;
 	
-	    public Session(int Session_Code, String Session_Type, String Description, int Max_participants , int Time , int Price , boolean Availability , int Trainer_Trainer_id , int GYM_Gym_code) {
+	    public Session(int Session_Code, String Session_Type, String Description, int Max_participants , int Time , int Price , boolean Availability , int Trainer_Trainer_id , int GYM_Gym_code , String date_And_Time) {
 	        this.Session_Code = Session_Code;
 	        this.Session_Type = Session_Type;
 	        this.Description = Description;
@@ -21,6 +23,8 @@
 	        this.Availability = Availability;
 	        this.Trainer_Trainer_id = Trainer_Trainer_id;
 	        this.GYM_Gym_code = GYM_Gym_code;
+			this.date_And_Time = date_And_Time;
+			this.amount_Of_Participants = 0;
 	    }
 	
 	    public int getSession_Code() {
@@ -101,4 +105,13 @@
 	    public void setGYM_Gym_code(int GYM_Gym_code) {
 	        this.GYM_Gym_code = GYM_Gym_code;
 	    }
+
+		public int getDate_And_Time() {
+	        return date_And_Time;
+	    }
+	
+	    public void setDate_And_Time(int date_And_Time) {
+	        this.date_And_Time = date_And_Time;
+	    }
+
 	}
