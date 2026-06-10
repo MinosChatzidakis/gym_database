fe1d528291e31e44b8cd9789ef84459aa00b5ec9
Aggelos06 <aggeloskarahalis@gmail.com>
2026-06-10 20:05:46 +0300

    Add files via upload

---
+++ java_code/q1-3/src/Gym_project/ServicesDBUtils.java
@@ -0,0 +1,55 @@
+package Gym_project;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public abstract class ServicesDBUtils {
+	public static ArrayList<Services> getServicesByGymCode(int gymCode){
+		String sql= "SELECT Service_Name FROM services WHERE GYM_Gym_code=" + "'" + gymCode + "'";
+		try (Connection con= SQLConnector.getConnection();
+				Statement stm= con.createStatement();){
+			ResultSet res= stm.executeQuery(sql);
+			ArrayList<Services> fetchedServices= new ArrayList<>();
+			while(res.next()) {
+				Services currentService= new Services(
+						res.getString("Service_name"),
+						res.getInt("GYM_Gym_code")
+												);
+				fetchedServices.add(currentService);
+			}
+			return fetchedServices;
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null; // nothing was found => we return null
+	}
+
+
+public static String getUnifiedServicesByGymCode(int gymCode){
+	String sql= "SELECT Service_Name FROM services WHERE GYM_Gym_code=" + "'" + gymCode + "'";
+	try (Connection con= SQLConnector.getConnection();
+			Statement stm= con.createStatement();){
+		ResultSet res= stm.executeQuery(sql);
+		ArrayList<Services> fetchedServices= new ArrayList<>();
+		while(res.next()) {
+			Services currentService= new Services(
+					res.getString("Service_name"),
+					res.getInt("GYM_Gym_code")
+											);
+			fetchedServices.add(currentService);
+		}
+		
+		StringBuilder sb= new StringBuilder();
+		for(Services s : fetchedServices) {
+			sb.append(s.getServiceName());
+		}
+		return sb.toString();
+	}catch(SQLException e) {
+		e.printStackTrace();
+	}
+	return ""; // nothing was found => we return null
+}
+}
